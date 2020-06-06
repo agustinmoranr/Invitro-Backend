a6b548ef860ade6fa06be10bc64f367203fe0d5f
agustin <agustin.moranr@gmail.com>
2020-06-05 19:38:45 -0500

    Invitro proyect base configuration

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express()
+const { config } = require('./config/index')
+
+const port = config.port || 8000
+
+// middlewarea
+app.use(express.json())
+
+//routes
+
+
+// server
+app.listen(port || 8000, () => { 
+        console.log(`Server created Successfully on ${port}`) 
+})
---
+++ store/firestore.js
@@ -0,0 +1,15 @@
+const admin = require('firebase-admin')
+const { config } = require('../config')
+
+//const serviceAccount = require(config);
+
+admin.initializeApp({
+  credential: admin.credential.cert(config),
+  databaseURL: "https://in-vitro-470ae.firebaseio.com",
+});
+
+const db = admin.firestore();
+
+module.exports = {
+    //instances
+}
