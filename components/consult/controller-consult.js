579d37d1ca4cc3b9f76273a80a0004dab5a47b95
agustin <agustin.moranr@gmail.com>
2020-07-07 16:59:53 -0500

    almacenando examenes como objetos map en firestore

--- components/consult/controller-consult.js
+++ components/consult/controller-consult.js
@@ -8,13 +8,34 @@ class Consult {
     
     async createConsult(data) {
         let uid = data.identityCard;
-        let exams = [];
+        //let examsArray = [];
         let consult = {};
         
         //defining Id Consult Document
         let date = new Date();
         let dateId = `${date.getDate()}-${(date.getMonth() + 1)}-${date.getFullYear()}-${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
         
+        //handler to set data
+        const setConsultData = async (uid, dateId, data) => {
+            return await this.db.collection('clinicHistory').doc(uid).collection('consults').doc(dateId).set(data)
+            .then(() => {
+                return console.log('New consult record created');
+            })
+            .catch((err) => {
+                console.error('Error on consultCreation',err);
+            });
+        };
+
+        const setExamData = async (uid, dateId, data) => {
+            return await this.db.collection('clinicHistory').doc(uid).collection('consults').doc(dateId).update(data)
+            .then(() => {
+                return console.log('New consult record created');
+            })
+            .catch((err) => {
+                console.error('Error on consultCreation',err);
+            });
+        };
+
         // If no exams, consult info changes
         if(data.exams.type === []) {
             consult = {
@@ -24,43 +45,44 @@ class Consult {
                     indications: data.indications
                 }
             };
+
+            return await setConsultData(uid, dateId, consult);
         }
 
         else {
-            let typeExam = data.exams.map((type) => {
-                return type;
-            });
-    
-            typeExam.forEach((exam) => {
-                let type = exam.type;
-
-                return exams.push({
-                    typeExam: type,
-                    status: "pending",
-                    pdfURL: null,
-                    examId: nanoid()
-                });
-            });
-
             consult = {
                 consultData: {
                     date: new Date().toDateString(),
                     aditionalData: data.aditionalData || null,
                     indications: data.indications
                 },
-                exams: exams
             };
-            //console.log(consult);
+
+            await setConsultData(uid, dateId, consult);
+
+            let typeExam = data.exams.map((type) => {
+                return type;
+            });
+    
+            typeExam.forEach(async (exam) => {
+                let examsData = {};
+                let type = exam.type;
+                let exams = nanoid();
+                let id = `'exam-${exams}'`;
+
+                examsData = {
+                    [id]: {
+                        typeExam: type,
+                        status: "pending",
+                        pdfURL: null,
+                        examId: exams
+                    },
+                };
+                
+                await setExamData(uid, dateId, examsData);
+            });
         }
-        
-        //set consult as a document
-        return await this.db.collection('clinicHistory').doc(uid).collection('consults').doc(dateId).set(consult)
-        .then(() => {
-            return console.log('New consult record created');
-        })
-        .catch((err) => {
-            console.error('Error on consultCreation',err);
-        });
+        return true;
     }
 }
 
