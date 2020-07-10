e4475c29ac3b55926a2197a31317145305d13151
agustin <agustin.moranr@gmail.com>
2020-07-09 23:15:13 -0500

    Adding missed params for csv user creatin. And updating methos to also assign a Exams and clinicHistory doc collection

--- components/user-massive/network-user-massive.js
+++ components/user-massive/network-user-massive.js
@@ -3,11 +3,13 @@ const { massive } = require("../../store/firestoreAdmin");
 const router = express.Router();
 const multer = require("multer");
 const csv = require('csvtojson');
+const fs = require('fs');
 
 const csvfilename = `Users-${Date.now()}.csv`;
+  
+  // define name and path where storage csv document
   var storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      // Uploads is the Upload_folder_name
       cb(null, "tempFiles");
     },
     filename: function (req, file, cb) {
@@ -16,8 +18,9 @@ const csvfilename = `Users-${Date.now()}.csv`;
   });
 
   // Define the maximum size for uploading
-  const maxSize = 1e6;
+  const maxSize = 500 * 1024; //500 Kbytes
 
+  // Request file options
   var upload = multer({
     storage: storage,
     limits: { fileSize: maxSize },
@@ -25,16 +28,26 @@ const csvfilename = `Users-${Date.now()}.csv`;
 
   router.post("/", (req, res, next) => {
     let users;
+
     upload(req, res, (err) => {
       if(err) {
         return next(err);
       } else {
+
+        // converts a csv file to JSON by file path. 
         csv()
         .fromFile(`./tempFiles/${csvfilename}`)
+
+        //Build users
         .then(async (json) => {
           users = await massive.insertUsers(json);
 
-          //fs.unlinkSync(`../../temp/${csvfilename}`);
+          // delete csv file stored previously on tempFiles
+          fs.unlink(`tempFiles/${csvfilename}`, (err) => {
+            if(err) throw err;
+            console.log(`./tempFiles/${csvfilename} was deleted`);
+          });
+
           return res.status(201).json({
             data: users,
             message: "Users created correctly"
