810ac0e931a92035692dcc806cbc41e9e3498fa6
jeisson <jmisnaza@hopper.com>
2020-07-14 20:55:04 -0500

    add login

--- components/login/controller-login.js
+++ components/login/controller-login.js
@@ -2,26 +2,54 @@
 var firebase = require("firebase/app");
 require("firebase/auth");
 
-var {firebaseSimple} = require('../../config/firebaseConfig')
+var { firebaseSimple } = require("../../config/firebaseConfig");
 
 firebase.initializeApp(firebaseSimple);
 
 class Login {
-    constructor(db) {
-        this.db = db
-        this.collection = this.db.collection('user')
-    }
-
-    async singIn(email, password){
-        const singIn = firebase.auth().signInWithEmailAndPassword(email,password)
-        .then(function(userRecord){
-            console.log('Successfully fetched user data:', userRecord.user.email);
-        }).catch(function(error) {
-            console.log('Error fetching user data:', error);
+  constructor(db) {
+    this.db = db;
+    this.collection = this.db.collection("user");
+  }
+  
+  async returnRol(email) {
+    let rol = ["data"]
+    this.collection.where("email", "==", email).get()
+      .then(snapshot => {
+        if (snapshot.empty) {
+          return console.log("No matching userDocuments.");
+        }
+        return snapshot.forEach((doc) => {
+            console.log( doc._fieldsProto.rol.stringValue )
+            return rol.push({
+                data: doc.data()             
+            })
         });
-        console.log(singIn)
-        return singIn
-    }
+      })
+      .catch((err) => {
+          console.log('Error getting documents', err)
+      })
+    return rol
+  }
+
+  async singIn(email, password) {
+    const singIn = firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(function (userRecord) {
+        return {
+          code: 200,
+          message: userRecord.user.email,
+        };
+      })
+      .catch(function (error) {
+        return {
+          code: 404,
+          message: error.message,
+        };
+      });
+    return singIn;
+  }
 }
 
-module.exports = Login
+module.exports = Login;
